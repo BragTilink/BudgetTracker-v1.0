3796c95a42480310e8df371460cabe2c88f856c8
BragTilink <thiago.palu17@gmail.com>
2026-08-10 20:00:33 -0300

    Database integration.

--- dashboard/cadastro.php
+++ dashboard/cadastro.php
@@ -1,8 +1,20 @@
 <?php 
-    session_start();
+    require_once "../database/database.php";
 
-    $_SESSION['nome'];
-    $_SESSION['telefone'];
-    $_SESSION['renda'];
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+    $salario = $_POST['renda'];
+    $telefone = $_POST['telefone'];
+
+    $sql = "INSERT INTO cadastro (nome, email, senha, salario, telefone)
+        VALUES ('$nome', '$email', '$senha', '$salario', '$telefone')";
+
+    $conexao->query($sql);
+
+    if ($conexao->query($sql)) {
+    header("Location: home.html");
+    exit;
+    }
     
 ?>
