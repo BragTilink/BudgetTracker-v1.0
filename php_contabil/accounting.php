262e11863eb634b363e60b6160fbeffbe5de0ec2
BragTilink <thiago.palu17@gmail.com>
2026-08-10 18:08:24 -0300

    Start HTML and PHP files

---
+++ php_contabil/accounting.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Confirmação de lançamento</title>
+    <link rel="stylesheet" href="../dashboard/style.css">
+</head>
+<body>
+    <?php 
+        session_start();
+
+        $responsavel = $_GET['responsavel'];
+        $valor = $_GET['valor'];
+        $tipo = $_GET['tipo_transacao'];
+        
+    ?>
+</body>
+</html>
